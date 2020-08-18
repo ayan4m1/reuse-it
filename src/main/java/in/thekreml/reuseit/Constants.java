e7bac9f33faae4860a8d4f4baa0ca8758030c54d
ayan4m1 <andrew@bulletlogic.com>
2020-08-18 18:51:20 -0400

    fix config path

--- src/main/java/in/thekreml/reuseit/Constants.java
+++ src/main/java/in/thekreml/reuseit/Constants.java
@@ -1,7 +1,7 @@
 package in.thekreml.reuseit;
 
 public class Constants {
-  public static final String CONFIG_PATH = "./plugins/rent-it/config.yml";
+  public static final String CONFIG_PATH = "./plugins/reuse-it/config.yml";
   public static final String CONFIG_KEY_MATERIALS = "materials";
   public static final String CONFIG_ERROR_EMPTY_MATERIALS = "Materials list should not be empty!";
 }
